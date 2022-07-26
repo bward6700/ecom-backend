118f30ef6247e60d2014685be3367dc848037fee
Administrator <bward6700@gmail.com>
2022-07-26 18:19:39 -0400

    server update

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const dotenv = require('dotenv');
 const mysql = require('mysql2');
-dotenv.config();
+
 
 app.use(cors());
 app.use(express.json());
